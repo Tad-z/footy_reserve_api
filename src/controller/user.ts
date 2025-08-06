f6678c0e5f2457f006ba987dfec2e855524c8c41
Tad-z <david.akintade1000@gmail.com>
2025-08-06 15:04:27 +0100

    log in

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -104,3 +104,111 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+        return res.status(400).json({
+        message: "Email and password are required",
+        });
+    }
+    
+    try {
+        const user = await User.findOne({ email });
+    
+        if (!user) {
+        return res.status(404).json({
+            message: "User not found",
+        });
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+        if (!isPasswordValid) {
+        return res.status(401).json({
+            message: "Invalid credentials",
+        });
+        }
+        
+        const jwtKey = process.env.JWT_KEY;
+        if (!jwtKey) {
+            throw new Error("JWT secret key is missing in environment variables.");
+        }
+        const accessToken = generateAccessToken(user, jwtKey);
+        const refreshToken = generateRefreshToken(user, jwtKey);
+
+        const deviceToken = req.body.deviceToken;
+        if (deviceToken) {
+        if (!user.deviceToken) {
+            user.deviceToken = [];
+        }
+        if (!user.deviceToken.includes(deviceToken)) {
+            user.deviceToken.push(deviceToken);
+        }
+        }
+    
+        user.refreshToken = refreshToken;
+        await user.save();
+    
+        return res.status(200).json({
+        message: "Login successful",
+        accessToken,
+        refreshToken,
+        user: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            country: user.country,
+            image: user.image,
+        },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+        message: "Internal server error",
+        });
+    }
+}
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const { token } = req.body;
+  if (!token) {
+    return res.status(401).json({
+      message: "Refresh token is required",
+    });
+  }
+
+  try {
+    const jwtKey = process.env.JWT_KEY;
+    if (!jwtKey) {
+      throw new Error("JWT secret key is missing in environment variables.");
+    }
+
+    const decoded = jwt.verify(token, jwtKey) as any;
+    const user = await User.findById(decoded.userID).exec();
+    if (!user || user.refreshToken !== token) {
+      return res.status(403).json({
+        message: "Invalid refresh token",
+      });
+    }
+
+    const newAccessToken = generateAccessToken(user, jwtKey);
+    const newRefreshToken = generateRefreshToken(user, jwtKey);
+
+    // Update refreshToken in the database or secure storage
+    user.refreshToken = newRefreshToken;
+
+    await user.save();
+
+    return res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({
+      message: "Invalid refresh token",
+    });
+  }
+};
